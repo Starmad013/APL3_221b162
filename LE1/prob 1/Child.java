05a0412d5a9ceb5c972ade525eea741355da3d93
Gauravjat1304 <115712903+Starmad013@users.noreply.github.com>
2025-01-28 12:02:31 +0530

    Delete LE1/prob 1 directory

--- LE1/prob 1/Child.java
+++
@@ -1,6 +0,0 @@
-class Child extends Mother {
-    @Override
-    void show() {
-        System.out.println("Hello JUET");
-    }
-}
--- LE1/prob 1/Mother.java
+++
@@ -1,5 +0,0 @@
-class Mother {
-    void show() {
-        System.out.println("Hello World");
-    }
-}
